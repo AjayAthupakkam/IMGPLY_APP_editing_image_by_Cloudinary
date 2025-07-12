cf1113ab4f82562505931ab2848366d8c9095a77
AjayAthupakkam <user@example.com>
2025-07-12 12:39:32 +0530

    commited the types

---
+++ types/index.ts
@@ -0,0 +1,144 @@
+/* eslint-disable no-unused-vars */
+
+// Remove User Params types
+/*
+declare type CreateUserParams = {
+  clerkId: string;
+  email: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  photo: string;
+};
+
+declare type UpdateUserParams = {
+  firstName: string;
+  lastName: string;
+  username: string;
+  photo: string;
+};
+*/
+
+// ====== IMAGE PARAMS
+export type AddImageParams = {
+  image: {
+    title: string;
+    publicId: string;
+    transformationType: string;
+    width: number;
+    height: number;
+    config: any;
+    secureURL: string;
+    transformationURL: string;
+    aspectRatio: string | undefined;
+    prompt: string | undefined;
+    color: string | undefined;
+  };
+  userId: string;
+  path: string;
+};
+
+export type UpdateImageParams = {
+  image: {
+    _id: string;
+    title: string;
+    publicId: string;
+    transformationType: string;
+    width: number;
+    height: number;
+    config: any;
+    secureURL: string;
+    transformationURL: string;
+    aspectRatio: string | undefined;
+    prompt: string | undefined;
+    color: string | undefined;
+  };
+  userId: string;
+  path: string;
+};
+
+export type Transformations = {
+  restore?: boolean;
+  fillBackground?: boolean;
+  remove?: {
+    prompt: string;
+    removeShadow?: boolean;
+    multiple?: boolean;
+  };
+  recolor?: {
+    prompt?: string;
+    to: string;
+    multiple?: boolean;
+  };
+  removeBackground?: boolean;
+};
+
+// ====== URL QUERY PARAMS
+export type FormUrlQueryParams = {
+  searchParams: string;
+  key: string;
+  value: string | number | null;
+};
+
+export type UrlQueryParams = {
+  params: string;
+  key: string;
+  value: string | null;
+};
+
+export type RemoveUrlQueryParams = {
+  searchParams: string;
+  keysToRemove: string[];
+};
+
+export type SearchParamProps = {
+  params: { id: string; type: TransformationTypeKey };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+import type { IImage } from "@/lib/database/models/image.model";
+
+export type TransformationFormProps = {
+  action: "Add" | "Update";
+  type: TransformationTypeKey;
+  data?: IImage | null;
+  config?: Transformations | null;
+  userId: string;
+};
+
+export type TransformedImageProps = {
+  image: any;
+  type: string;
+  title: string;
+  transformationConfig: Transformations | null;
+  isTransforming: boolean;
+  hasDownload?: boolean;
+  setIsTransforming?: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+// Remove Transaction Params types
+/*
+declare type CheckoutTransactionParams = {
+  plan: string;
+  credits: number;
+  amount: number;
+  buyerId: string;
+};
+
+declare type CreateTransactionParams = {
+  stripeId: string;
+  amount: number;
+  credits: number;
+  plan: string;
+  buyerId: string;
+  createdAt: Date;
+};
+*/
+
+export type TransformationTypeKey =
+  | "restore"
+  | "fill"
+  | "remove"
+  | "recolor"
+  | "removeBackground"
+  | "replace";
